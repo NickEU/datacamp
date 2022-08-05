af192e85c0b957546e44f0e8624c861ed3429215
NickEU <k@nick.tf>
2022-08-05 12:37:27 +0300

    feat(time-series-manip): sp500 price sim: random stock price path
    Signed-off-by: NickEU <k@nick.tf>

--- datacamp/time-series-pandas/sp500_price_sim/random_walk_b_historical_returns.py
+++ datacamp/time-series-pandas/sp500_price_sim/random_walk_b_historical_returns.py
@@ -3,16 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fb_stock_price_history = pd.read_csv('../stock_data/fb.csv', parse_dates=[0], index_col=0, header=None, names=['date', 'price'])
+fb_stock_price_history = pd.read_csv('../stock_data/fb.csv', parse_dates=[0], index_col=0,
+                                     header=None, names=['date', 'price'])
 
 daily_returns = fb_stock_price_history.pct_change().dropna()
 count_observations = daily_returns.price.count()
 
-seed(42)
+seed(11)
 random_walk = choice(daily_returns.price, count_observations)
 
 random_walk = pd.Series(random_walk)
 
 # Plot random_walk distribution
-sns.histplot(random_walk, kde=True, stat="density", linewidth=0)
-plt.show()
+if __name__ == "__main__":
+    sns.histplot(random_walk, kde=True, stat="density", linewidth=0)
+    plt.show()
